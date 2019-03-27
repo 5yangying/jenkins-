ace156acb202b796d2c19dfd37643aaba574de5e
laoyang <123456@136.com>
2019-03-27 19:29:53 +0800

    第一次提交

---
+++ script/test_foo.py
@@ -0,0 +1,12 @@
+class TestFoo:
+    def test_fun1(self):
+        print('执行函数1')
+        assert 1
+
+    def test_fun2(self):
+        print('执行函数2')
+        assert 0
+
+    def test_fun3(self):
+        print('执行函数3')
+        assert 1
